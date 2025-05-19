b34a846c4c26266110b81fc8dc69ed06efe26fa5
Eduu115 <serranotrenado.e@gmail.com>
2025-05-19 10:08:02 +0200

    initial commit

---
+++ Eventos_FE_Profe/src/main/java/eventosweb/EventosFeProfeApplication.java
@@ -0,0 +1,13 @@
+package eventosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosFeProfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosFeProfeApplication.class, args);
+	}
+
+}
