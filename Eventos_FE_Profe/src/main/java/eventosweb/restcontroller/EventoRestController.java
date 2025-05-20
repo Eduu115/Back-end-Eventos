4fd1b990a30ba72c267e248a6d29edd79c37cfae
Eduu115 <serranotrenado.e@gmail.com>
2025-05-20 14:11:14 +0200

    create and fixes at create event rest

--- Eventos_FE_Profe/src/main/java/eventosweb/restcontroller/EventoRestController.java
+++ Eventos_FE_Profe/src/main/java/eventosweb/restcontroller/EventoRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,10 +46,8 @@ public class EventoRestController {
 	public int eliminar(@PathVariable Integer idEvento) {
 		return edao.deleteById(idEvento);
 	}
-	/*
-	@PostMapping("/alta")
-	public Evento alta(@RequestBody Evento evento){
+	@PostMapping("/crearEvento")
+	public Evento alta(@RequestBody Evento evento) {
 		return edao.insertOne(evento);
 	}
-	*/
 }
